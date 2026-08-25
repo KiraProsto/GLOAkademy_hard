b08af2bb1009da294895f96fa85bca88422e68a7
Lugovskaya_Kira <kiraprosto0.0@gmail>
2026-08-25 19:22:52 +0300

    commit refactor lessons into separate files

--- index3.js
+++ index3.js
@@ -1,21 +1,3 @@
-'strict';
-// lesson02
-// const num = 266219;
-// const digits = num.toString().split('').map(Number);
-
-// let product = 1;
-// for (let i = 0; i < digits.length; i++) {
-//   product *= digits[i];
-// }
-
-// product = (product ** 3).toString();
-// console.log(product.substring(0, 2));
-
-//через reduce
-// product = digits.reduce((acc, digit) => acc * digit, 1);
-// product = product = (product ** 3).toString();
-// console.log(product.substring(0, 2));
-
 // lesson03
 const ru_week =
   'Понедельник\nВторник\nСреда\nЧетверг\nПятница\nСуббота\nВоскресенье';

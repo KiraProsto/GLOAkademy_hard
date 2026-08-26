f768f9db6784ff08c68dd85735e74d91bed9f65a
Lugovskaya_Kira <kiraprosto0.0@gmail>
2026-08-26 22:17:41 +0300

    completed second task l05

--- index5.js
+++ index5.js
@@ -23,3 +23,28 @@ if (isNum(input)) {
 } else {
   console.log('Введено не число или нажата отмена');
 }
+
+//Z2
+let arr = ['123456', '2345678', '345678', '456789', '67890', '98765', '87654'];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i].charAt(0) === '2' || arr[i].charAt(0) === '4') {
+    console.log(arr[i]);
+  }
+}
+
+//Z2
+for (let n = 2; n <= 100; n++) {
+  let isPrime = true;
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+
+  if (isPrime) {
+    console.log(n);
+    console.log(`Делители этого числа: 1 и ${n}`);
+  }
+}
